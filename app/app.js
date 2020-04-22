40902b0dc9d0d0aeefa6487d2e3c54b22e69257e
agcamacho <anthony.g.camacho@mail.com>
2020-04-21 21:02:53 -0700

    docker-compose looks good, next setup api placeholder

--- app/app.js
+++ app/app.js
@@ -1,81 +1,41 @@
-'use strict';
 
-const Hapi = require('@hapi/hapi');
 
-const server = Hapi.server({
-    port: 3000,
-    host: '0.0.0.0'
+
+// place this code in your node app, ideally in index.js or ./bin/www
+// 
+// you need this code so node will watch for exit signals
+// node by default doesn't handle SIGINT/SIGTERM
+// docker containers use SIGINT and SIGTERM to properly exit
+//
+// signals also aren't handeled by npm:
+// https://github.com/npm/npm/issues/4603
+// https://github.com/npm/npm/pull/10868
+// https://github.com/RisingStack/kubernetes-graceful-shutdown-example/blob/master/src/index.js
+// if you want to use npm then start with `docker run --init` to help, but I still don't think it's
+// a graceful shutdown of node process, just a forced exit
+//
+
+// quit on ctrl-c when running docker in terminal
+process.on('SIGINT', function onSigint () {
+	console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+    shutdown();
 });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
+// quit properly on docker stop
+process.on('SIGTERM', function onSigterm () {
+    console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+    shutdown();
+})
 
-        request.logger.info('In handler %s', request.path);
-        return 'Hello, world!';
-    }
-});
-
-const init = async () => {
-
-     await server.register({
-        plugin: require('hapi-pino'),
-        options: {
-            prettyPrint: true,
-            logEvents: ['response', 'onPostStart']
+// shut down server
+function shutdown() {
+  // NOTE: server.close is for express based apps
+  // If using hapi, use `server.stop`
+    server.close(function onServerClosed (err) {
+    if (err) {
+        console.error(err);
+        process.exitCode = 1;
         }
-    });
-
-
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
-};
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
-
-init();
-
-
-// // place this code in your node app, ideally in index.js or ./bin/www
-// // 
-// // you need this code so node will watch for exit signals
-// // node by default doesn't handle SIGINT/SIGTERM
-// // docker containers use SIGINT and SIGTERM to properly exit
-// //
-// // signals also aren't handeled by npm:
-// // https://github.com/npm/npm/issues/4603
-// // https://github.com/npm/npm/pull/10868
-// // https://github.com/RisingStack/kubernetes-graceful-shutdown-example/blob/master/src/index.js
-// // if you want to use npm then start with `docker run --init` to help, but I still don't think it's
-// // a graceful shutdown of node process, just a forced exit
-// //
-
-// // quit on ctrl-c when running docker in terminal
-// process.on('SIGINT', function onSigint () {
-// 	console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
-//     shutdown();
-// });
-
-// // quit properly on docker stop
-// process.on('SIGTERM', function onSigterm () {
-//     console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
-//     shutdown();
-// })
-
-// // shut down server
-// function shutdown() {
-//   // NOTE: server.close is for express based apps
-//   // If using hapi, use `server.stop`
-//     server.close(function onServerClosed (err) {
-//     if (err) {
-//         console.error(err);
-//         process.exitCode = 1;
-//         }
-//         process.exit();
-//     })
-// }
+        process.exit();
+    })
+}
